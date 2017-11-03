35a2fa900e4624849e747a6944a8efbdb52e93b8
Will <Will@DESKTOP-E6E62EP>
2017-11-03 14:14:55 -0600

    Opcion Jugadores

--- src/lab4_richardson_wilfredo/Lab4_Richardson_Wilfredo.java
+++ src/lab4_richardson_wilfredo/Lab4_Richardson_Wilfredo.java
@@ -5,28 +5,15 @@ import javax.swing.JOptionPane;
 
 public class Lab4_Richardson_Wilfredo {
     static ArrayList<Jugadores> jugadores=new ArrayList();
+    static ArrayList<Guerreros> guerreros=new ArrayList();
+    
     public static void main(String[] args) {
-        
-        switch (menu()){
-            case 1:
-                break;
-            case 2:
-                break;
-            case 3:
-                RegistroJugadores();
-                break;
-            case 4:
-                break;
-            case 5:
-                break;
-            case 6:
-                break;
-            default:
-                break;
-        }
+        menu();
     }
-    public static int menu(){
-        String [] opciones = {
+    public static void menu(){
+        String resp="";
+        while(!resp.equals("Salir")){
+            String [] opciones = {
             "1.- Creacion de Guerreros",
             "2.- Eliminación de Guerreros",
             "3.- Registro Jugadores",
@@ -35,18 +22,49 @@ public class Lab4_Richardson_Wilfredo {
             "6.- Cargar Partidas",
             "Salir"
         };
-        String resp = (String) JOptionPane.showInputDialog(null, 
+        resp = (String) JOptionPane.showInputDialog(null, 
                             "Seleccione una opción", "MENÚ PRINCIPAL", 
                             JOptionPane.DEFAULT_OPTION, null, opciones, opciones[0]);
-        int op = Integer.parseInt(resp.charAt(0)+"");
-        return op;
+        if (resp.equals("1.- Creacion de Guerreros")) {
+            
+        }
+        if (resp.equals("2.- Eliminación de Guerreros")) {
+            
+        }
+        if (resp.equals("3.- Registro Jugadores")) {
+            RegistroJugadores();
+        }
+        if (resp.equals("4.- Iniciar Partida")) {
+            
+        }
+        if (resp.equals("5.- Listar Jugadores")) {
+            ListarJugadores();
+        }
+        if (resp.equals("6.- Cargar Partidas")) {
+            
+        }
+        }
     }
     
     public static void RegistroJugadores(){
+        jugadores.add(new Jugadores());
         String Nombre=JOptionPane.showInputDialog("Ingrese el Nombre del Jugador:"); 
-        int DineroDisponible=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Dinero Disponible:")); 
+        jugadores.get(jugadores.size()-1).setNombre(Nombre);
+        int DineroDisponible=Integer.parseInt(JOptionPane.showInputDialog("Ingrese el Dinero Disponible:"));
+        jugadores.get(jugadores.size()-1).setDineroDisponible(DineroDisponible);
         int Puntos=0;
-        int PosicionGuerrero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la posición a eliminar:"));
+        jugadores.get(jugadores.size()-1).setPuntos(Puntos);
+        int PosicionGuerrero=Integer.parseInt(JOptionPane.showInputDialog("Ingrese la Poscion de Guerrero:"));
+        Guerreros Guerrero=guerreros.get(PosicionGuerrero);
+        jugadores.get(jugadores.size()-1).setGuerrero(Guerrero);
+    }
+    
+    public static void ListarJugadores(){
+        String s="________________________________________Personas________________________________________\n";
+        for (Object t : jugadores) {
+        s+=""+jugadores.indexOf(t)+""+") \n"+t+"\n\n";    
+        }
+        JOptionPane.showMessageDialog(null, s);
     }
     
     
