d5201bb7d3a329bfab5055b445df3d7d87faba45
RALC365 <alejandro.carcamo@unitec.edu>
2017-11-03 08:17:11 -0600

    Prueba

---
+++ src/lab4_richardson_wilfredo/Lab4_Richardson_Wilfredo.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package lab4_richardson_wilfredo;
+
+/**
+ *
+ * @author RALC
+ */
+public class Lab4_Richardson_Wilfredo {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
